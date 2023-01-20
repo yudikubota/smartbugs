d10dff2a31bfe22016e4c6ec56c0d2930b5efbe6
Yudi Kubota <yudikubota@gmail.com>
2023-01-20 15:09:34 -0300

    tcc3

--- tcc.sh
+++ tcc.sh
@@ -1,34 +1,37 @@
-echo "0x0*.sol"
-./smartbugs --processes 1 -f ../smartbugs-wild/output/0x0*.sol --timeout 600 -t mythril --results './results/${RUNID}/${FILENAME}/${TOOL}' --sarif --log './results/${RUNID}.log' --runid fix_run
-echo "0x1*.sol"
-./smartbugs --processes 1 -f ../smartbugs-wild/output/0x1*.sol --timeout 600 -t mythril --results './results/${RUNID}/${FILENAME}/${TOOL}' --sarif --log './results/${RUNID}.log' --runid fix_run
-echo "0x2*.sol"
-./smartbugs --processes 1 -f ../smartbugs-wild/output/0x2*.sol --timeout 600 -t mythril --results './results/${RUNID}/${FILENAME}/${TOOL}' --sarif --log './results/${RUNID}.log' --runid fix_run
-echo "0x3*.sol"
-./smartbugs --processes 1 -f ../smartbugs-wild/output/0x3*.sol --timeout 600 -t mythril --results './results/${RUNID}/${FILENAME}/${TOOL}' --sarif --log './results/${RUNID}.log' --runid fix_run
-echo "0x4*.sol"
-./smartbugs --processes 1 -f ../smartbugs-wild/output/0x4*.sol --timeout 600 -t mythril --results './results/${RUNID}/${FILENAME}/${TOOL}' --sarif --log './results/${RUNID}.log' --runid fix_run
-echo "0x5*.sol"
-./smartbugs --processes 1 -f ../smartbugs-wild/output/0x5*.sol --timeout 600 -t mythril --results './results/${RUNID}/${FILENAME}/${TOOL}' --sarif --log './results/${RUNID}.log' --runid fix_run
-echo "0x6*.sol"
-./smartbugs --processes 1 -f ../smartbugs-wild/output/0x6*.sol --timeout 600 -t mythril --results './results/${RUNID}/${FILENAME}/${TOOL}' --sarif --log './results/${RUNID}.log' --runid fix_run
-echo "0x7*.sol"
-./smartbugs --processes 1 -f ../smartbugs-wild/output/0x7*.sol --timeout 600 -t mythril --results './results/${RUNID}/${FILENAME}/${TOOL}' --sarif --log './results/${RUNID}.log' --runid fix_run
-echo "0x8*.sol"
-./smartbugs --processes 1 -f ../smartbugs-wild/output/0x8*.sol --timeout 600 -t mythril --results './results/${RUNID}/${FILENAME}/${TOOL}' --sarif --log './results/${RUNID}.log' --runid fix_run
-echo "0x9*.sol"
-./smartbugs --processes 1 -f ../smartbugs-wild/output/0x9*.sol --timeout 600 -t mythril --results './results/${RUNID}/${FILENAME}/${TOOL}' --sarif --log './results/${RUNID}.log' --runid fix_run
-echo "0xa*.sol"
-./smartbugs --processes 1 -f ../smartbugs-wild/output/0xa*.sol --timeout 600 -t mythril --results './results/${RUNID}/${FILENAME}/${TOOL}' --sarif --log './results/${RUNID}.log' --runid fix_run
-echo "0xb*.sol"
-./smartbugs --processes 1 -f ../smartbugs-wild/output/0xb*.sol --timeout 600 -t mythril --results './results/${RUNID}/${FILENAME}/${TOOL}' --sarif --log './results/${RUNID}.log' --runid fix_run
-echo "0xc*.sol"
-./smartbugs --processes 1 -f ../smartbugs-wild/output/0xc*.sol --timeout 600 -t mythril --results './results/${RUNID}/${FILENAME}/${TOOL}' --sarif --log './results/${RUNID}.log' --runid fix_run
-echo "0xd*.sol"
-./smartbugs --processes 1 -f ../smartbugs-wild/output/0xd*.sol --timeout 600 -t mythril --results './results/${RUNID}/${FILENAME}/${TOOL}' --sarif --log './results/${RUNID}.log' --runid fix_run
-echo "0xe*.sol"
-./smartbugs --processes 1 -f ../smartbugs-wild/output/0xe*.sol --timeout 600 -t mythril --results './results/${RUNID}/${FILENAME}/${TOOL}' --sarif --log './results/${RUNID}.log' --runid fix_run
-echo "0xf*.sol"
-./smartbugs --processes 1 -f ../smartbugs-wild/output/0xf*.sol --timeout 600 -t mythril --results './results/${RUNID}/${FILENAME}/${TOOL}' --sarif --log './results/${RUNID}.log' --runid fix_run
+N_PROCESSES=1
+SB_TOOLS="mythril"
 
-# ./smartbugs --processes 15 -f ../smartbugs-wild/output/0x2*.sol --timeout 600 -t mythril --results './results/${RUNID}/${FILENAME}/${TOOL}' --sarif --log './results/${RUNID}.log' --runid fix_run
+echo "0x0*.sol"
+./smartbugs --processes $N_PROCESSES -f ../smartbugs-wild/output/0x0*.sol --timeout 600 -t $SB_TOOLS --results './results/${RUNID}/${FILENAME}/${TOOL}' --sarif --log './results/${RUNID}.log' --runid fix_run
+echo "0x1*.sol"
+./smartbugs --processes $N_PROCESSES -f ../smartbugs-wild/output/0x1*.sol --timeout 600 -t $SB_TOOLS --results './results/${RUNID}/${FILENAME}/${TOOL}' --sarif --log './results/${RUNID}.log' --runid fix_run
+echo "0x2*.sol"
+./smartbugs --processes $N_PROCESSES -f ../smartbugs-wild/output/0x2*.sol --timeout 600 -t $SB_TOOLS --results './results/${RUNID}/${FILENAME}/${TOOL}' --sarif --log './results/${RUNID}.log' --runid fix_run
+echo "0x3*.sol"
+./smartbugs --processes $N_PROCESSES -f ../smartbugs-wild/output/0x3*.sol --timeout 600 -t $SB_TOOLS --results './results/${RUNID}/${FILENAME}/${TOOL}' --sarif --log './results/${RUNID}.log' --runid fix_run
+echo "0x4*.sol"
+./smartbugs --processes $N_PROCESSES -f ../smartbugs-wild/output/0x4*.sol --timeout 600 -t $SB_TOOLS --results './results/${RUNID}/${FILENAME}/${TOOL}' --sarif --log './results/${RUNID}.log' --runid fix_run
+echo "0x5*.sol"
+./smartbugs --processes $N_PROCESSES -f ../smartbugs-wild/output/0x5*.sol --timeout 600 -t $SB_TOOLS --results './results/${RUNID}/${FILENAME}/${TOOL}' --sarif --log './results/${RUNID}.log' --runid fix_run
+echo "0x6*.sol"
+./smartbugs --processes $N_PROCESSES -f ../smartbugs-wild/output/0x6*.sol --timeout 600 -t $SB_TOOLS --results './results/${RUNID}/${FILENAME}/${TOOL}' --sarif --log './results/${RUNID}.log' --runid fix_run
+echo "0x7*.sol"
+./smartbugs --processes $N_PROCESSES -f ../smartbugs-wild/output/0x7*.sol --timeout 600 -t $SB_TOOLS --results './results/${RUNID}/${FILENAME}/${TOOL}' --sarif --log './results/${RUNID}.log' --runid fix_run
+echo "0x8*.sol"
+./smartbugs --processes $N_PROCESSES -f ../smartbugs-wild/output/0x8*.sol --timeout 600 -t $SB_TOOLS --results './results/${RUNID}/${FILENAME}/${TOOL}' --sarif --log './results/${RUNID}.log' --runid fix_run
+echo "0x9*.sol"
+./smartbugs --processes $N_PROCESSES -f ../smartbugs-wild/output/0x9*.sol --timeout 600 -t $SB_TOOLS --results './results/${RUNID}/${FILENAME}/${TOOL}' --sarif --log './results/${RUNID}.log' --runid fix_run
+echo "0xa*.sol"
+./smartbugs --processes $N_PROCESSES -f ../smartbugs-wild/output/0xa*.sol --timeout 600 -t $SB_TOOLS --results './results/${RUNID}/${FILENAME}/${TOOL}' --sarif --log './results/${RUNID}.log' --runid fix_run
+echo "0xb*.sol"
+./smartbugs --processes $N_PROCESSES -f ../smartbugs-wild/output/0xb*.sol --timeout 600 -t $SB_TOOLS --results './results/${RUNID}/${FILENAME}/${TOOL}' --sarif --log './results/${RUNID}.log' --runid fix_run
+echo "0xc*.sol"
+./smartbugs --processes $N_PROCESSES -f ../smartbugs-wild/output/0xc*.sol --timeout 600 -t $SB_TOOLS --results './results/${RUNID}/${FILENAME}/${TOOL}' --sarif --log './results/${RUNID}.log' --runid fix_run
+echo "0xd*.sol"
+./smartbugs --processes $N_PROCESSES -f ../smartbugs-wild/output/0xd*.sol --timeout 600 -t $SB_TOOLS --results './results/${RUNID}/${FILENAME}/${TOOL}' --sarif --log './results/${RUNID}.log' --runid fix_run
+echo "0xe*.sol"
+./smartbugs --processes $N_PROCESSES -f ../smartbugs-wild/output/0xe*.sol --timeout 600 -t $SB_TOOLS --results './results/${RUNID}/${FILENAME}/${TOOL}' --sarif --log './results/${RUNID}.log' --runid fix_run
+echo "0xf*.sol"
+./smartbugs --processes $N_PROCESSES -f ../smartbugs-wild/output/0xf*.sol --timeout 600 -t $SB_TOOLS --results './results/${RUNID}/${FILENAME}/${TOOL}' --sarif --log './results/${RUNID}.log' --runid fix_run
+
+# ./smartbugs --processes $N_PROCESSES5 -f ../smartbugs-wild/output/0x2*.sol --timeout 600 -t $SB_TOOLS --results './results/${RUNID}/${FILENAME}/${TOOL}' --sarif --log './results/${RUNID}.log' --runid fix_run
